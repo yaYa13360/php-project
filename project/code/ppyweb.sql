3ceaf37a1cc21a63fa628da6fe112574a0a0b95d
yaYa13360 <83057633+yaYa13360@users.noreply.github.com>
2021-12-25 13:47:27 +0800

    re sql
    re ppy sql

--- project/code/ppyweb.sql
+++ project/code/ppyweb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-12-24 14:26:51
+-- 產生時間： 2021-12-25 06:46:54
 -- 伺服器版本： 10.4.21-MariaDB
--- PHP 版本： 7.4.24
+-- PHP 版本： 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫: `ppy_web`
+-- 資料庫: `ppyweb`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
-  `identity` text NOT NULL,
+  `identity` enum('user','creator') NOT NULL,
   `name` text NOT NULL,
   `email` text NOT NULL,
   `password` int(40) NOT NULL
@@ -41,8 +41,9 @@ CREATE TABLE `account` (
 INSERT INTO `account` (`identity`, `name`, `email`, `password`) VALUES
 ('user', 'ppy', 'ppy@gmail.com', 123),
 ('user', 'ppy2', 'ppy2@gmail.com', 123),
-('creater', 'ppy3', 'ppy3', 123),
-('creater', 'ppy4', 'ppy4@gmail.com', 123);
+('user', 'ppy3', 'ppy3', 123),
+('user', 'ppy4', 'ppy4@gmail.com', 123),
+('user', 'ppy5', 'ppy5@gmail.com', 123);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
