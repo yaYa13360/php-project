03d5ef4e0709e07d08c1290350c028ce79e7d4e5
yaYa13360 <83057633+yaYa13360@users.noreply.github.com>
2021-12-29 16:09:28 +0800

    gamelist
    add gameIcon gameQR

--- project/code/upLoadGame.php
+++ project/code/upLoadGame.php
@@ -7,7 +7,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 	$introduction=$_POST["introduction"];
 	$classification=$_POST["classification"];
 	
-	$sql="INSERT INTO gamelist (gameName,introduction,classification)VALUES('".$gameName."','".$introduction."','".$classification."')";       
+	$gameQR = $_FILES['gameQR']['tmp_name'];
+
+	$gameIcon = $_FILES['gameIcon']['tmp_name'];
+	
+	$data1 = addslashes(fread(fopen($gameQR, "r"), filesize($gameQR)));
+	$data2 = addslashes(fread(fopen($gameIcon, "r"), filesize($gameIcon)));
+	echo "mysqlPicture=".$data1;
+	$sql="INSERT INTO gamelist (gameName,introduction,classification,gameQR,gameIcon)
+			VALUES('".$gameName."','".$introduction."','".$classification."','".$data1."','".$data2."')";       
 	if(mysqli_query($conn, $sql)){
 		echo "<script> alert('上傳成功!');parent.location.href='personal.html'; </script>";
 		exit;
