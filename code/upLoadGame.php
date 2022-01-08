8a27c1728589db643d18024b8382899d03079b3f
yaYa13360 <83057633+yaYa13360@users.noreply.github.com>
2022-01-09 05:10:23 +0800

    re gameList sql
    design gameList

---
+++ code/upLoadGame.php
@@ -0,0 +1,41 @@
+<?php 
+require("upLoadGame.html");
+$conn=require_once("config.php"); //引入資料庫
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){	
+	$gameName=$_POST["gameName"];
+	$introduction=$_POST["introduction"];
+	$classification=$_POST["classification"];
+	
+	$gameQR = $_FILES['gameQR']['tmp_name'];
+
+	$gameIcon = $_FILES['gameIcon']['tmp_name'];
+	
+	$gamePic1 = $_FILES['gamePic1']['tmp_name'];
+	$gamePic2 = $_FILES['gamePic2']['tmp_name'];
+	$gamePic3 = $_FILES['gamePic3']['tmp_name'];
+	$gamePic4 = $_FILES['gamePic4']['tmp_name'];
+	$gamePic5 = $_FILES['gamePic5']['tmp_name'];
+	
+	$creatorName=$_POST["creatorName"];
+	
+	$data1 = addslashes(fread(fopen($gameQR, "r"), filesize($gameQR)));
+	$data2 = addslashes(fread(fopen($gameIcon, "r"), filesize($gameIcon)));
+	$pic1 = addslashes(fread(fopen($gamePic1, "r"), filesize($gamePic1)));
+	$pic2 = addslashes(fread(fopen($gamePic2, "r"), filesize($gamePic2)));
+	$pic3 = addslashes(fread(fopen($gamePic3, "r"), filesize($gamePic3)));
+	$pic4 = addslashes(fread(fopen($gamePic4, "r"), filesize($gamePic4)));
+	$pic5 = addslashes(fread(fopen($gamePic5, "r"), filesize($gamePic5)));
+	
+	$sql="INSERT INTO gamelist (gameName,introduction,classification,gameQR,gameIcon,gamePic1,gamePic2,gamePic3,gamePic4,gamePic5,creatorName)
+			VALUES('".$gameName."','".$introduction."','".$classification."','".$data1."','".$data2."','".$pic1."','".$pic2."','".$pic3."','".$pic4."','".$pic5."','".$creatorName."')";      
+	if(mysqli_query($conn, $sql)){
+		echo "<script> alert('上傳成功!');parent.location.href='personal.html'; </script>";
+		exit;
+	}else{
+		echo "<script> alert('wrong!!!');</script>";
+	}
+	
+}
+mysqli_close($conn); //關閉資料庫
+?>
--- project/code/test.php
+++
@@ -1,7 +0,0 @@
-<!-- 測試是否能用網址傳值 -->
-<html>
-    <?php
-        $gameName="章魚吃葉葉";
-        echo "<a href='app.php?gameName=" . $gameName . "'>click</a>";
-    ?>
-</html>
